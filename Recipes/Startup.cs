becff1b5cddeb28ec4135584ab238b7ae81c98d4
Yordan Dyakov <yordan.dyakov@mentormate.com>
2017-11-21 19:24:32 +0200

    initial

---
+++ Recipes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Recipes.Startup))]
+namespace Recipes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
